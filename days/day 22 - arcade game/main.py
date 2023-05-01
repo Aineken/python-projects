ea5a04ef580942832e92b9849d9d8da79c2efc8c
Andrzul <aineken93@gmail.com>
2023-05-01 17:20:07 +0200

    the day 22 has started

---
+++ days/day 22 - arcade game/main.py
@@ -0,0 +1,13 @@
+from turtle import Screen
+
+
+
+screen = Screen()
+screen.setup(800, 600)
+screen.colormode(255)
+screen.bgcolor("black")
+screen.title("My Arcade Game")
+
+
+
+screen.exitonclick()
