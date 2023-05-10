672a37f618da9c6dd2aac27d169d855e5c9588c2
Andrzul <aineken93@gmail.com>
2023-05-10 15:13:57 +0200

    code wars - def nb_year(p0, percent, aug, p):

--- algorithm exercises/codewars 10may/main.py
+++ algorithm exercises/codewars 10may/main.py
@@ -1,18 +1,34 @@
-from six.moves import reduce
-
-def persistence(n):
-    res = 0
-    data = n
-    arr = []
-    while data > 9:
-        for i in str(data):
-            arr.append(int(i))
-        data = reduce(lambda x, y: x*y, arr)
-        arr = []
-
-        res +=1
-
-    return res
+# def nb_year(p0, percent, aug, p):
+# def nb_year(p0, percent, aug, p):
+#     pop = p0
+#     res = 0
+#     while pop < p:
+#         pop = pop * (percent+100)/100 + aug
+#         res += 1
+#
+#     return res
+#
+#
+# print(nb_year(1500, 5, 100, 5000))
+# # 15/
 
 
-print(persistence(17))
+# persistence(n):
+# from six.moves import reduce
+#
+# def persistence(n):
+#     res = 0
+#     data = n
+#     arr = []
+#     while data > 9:
+#         for i in str(data):
+#             arr.append(int(i))
+#         data = reduce(lambda x, y: x*y, arr)
+#         arr = []
+#
+#         res +=1
+#
+#     return res
+#
+#
+# print(persistence(17))
